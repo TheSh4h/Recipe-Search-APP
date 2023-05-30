fafe50c3b4d141cea2386fa98329d2a6a37eb70e
TheSh4h <hh.haseebshah@gmail.com>
2023-05-30 05:43:16 -0700

    First Release

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
-import { useState } from "react";
-import TodoInput from "./TodoInput";
-import TodoList from "./TodoList";
-
-function App(){
-
-    const [todos, setTodos] = useState([])
-    const [todo, setTodo] = useState("");
-
-    const addTodo = (e) => {
-        e.preventDefault();
-        if(todo !== "") {
-            setTodos([...todos, todo])
-            setTodo("");
-        }
-    }
-
-    const deleteTodo = (value) => {
-        const newTodo = todos.filter(todo => {
-           return todo !== value;
-        });
-        setTodos(newTodo);
-    }
-
-    document.title = "DODO APP";
-
+const App = () => {
     return ( 
-        <div className="text-center text-slate-200 text-xl p-10
-        font-mono font-bold">
-            <h1>DO-DO APP</h1>
-            <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
-            <TodoList list={todos} remove={deleteTodo} />
+        <div>
+            
         </div>
      );
 }
